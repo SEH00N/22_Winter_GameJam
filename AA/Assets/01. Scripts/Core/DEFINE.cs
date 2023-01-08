6a422018775e9f8deb6fd28cbf3a36e2ac1ffbfd
SEH00N <06psh0501@gmail.com>
2023-01-08 23:58:55 +0900

    core script init

---
+++ AA/Assets/01. Scripts/Core/DEFINE.cs
@@ -0,0 +1,25 @@
+using Cinemachine;
+using UnityEngine;
+
+public class DEFINE
+{    
+    private static Camera mainCam = null;
+    public static Camera MainCam {
+        get {
+            if(mainCam == null)
+                mainCam = Camera.main;
+
+            return mainCam;
+        }
+    }
+
+    private static CinemachineVirtualCamera cmMainCam = null;
+    public static CinemachineVirtualCamera CmMainCam  {
+        get {
+            if(cmMainCam == null)
+                cmMainCam = GameObject.Find("CmMainCam").GetComponent<CinemachineVirtualCamera>();
+
+            return cmMainCam;
+        }
+    }
+}
---
+++ AA/Assets/01. Scripts/Core/GameManager.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance = null;
+    public static GameManager Instance {
+        get {
+            if(instance == null)
+                instance = FindObjectOfType<GameManager>();
+
+            return instance;
+        }
+    }
+
+    [SerializeField] bool cursorActive = false;
+    public bool CursorActive {
+        get => cursorActive;
+        set {
+            cursorActive = value;
+            Cursor.visible = cursorActive;
+            Cursor.lockState = cursorActive ? CursorLockMode.None : CursorLockMode.Locked;
+        }
+    }
+
+    private void Awake()
+    {
+        if(instance != null) { Debug.LogWarning("Multiple gameManager instance is running, destroy this"); Destroy(gameObject); }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        CursorActive = cursorActive;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.LeftAlt))
+            if(Input.GetKey(KeyCode.LeftShift))
+                if(Input.GetKeyDown(KeyCode.Comma))
+                    CursorActive = !CursorActive;
+    }
+}
