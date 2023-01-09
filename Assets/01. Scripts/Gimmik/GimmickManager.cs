a2aa9d279a4fb9110b6907daf345ce27eb5241c2
hueam <72968569+hueam@users.noreply.github.com>
2023-01-09 20:21:08 +0900

    gimmick manager

---
+++ Assets/01. Scripts/Gimmik/GimmickManager.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GimmickManager : MonoBehaviour
+{
+    private static GimmickManager instance = null;
+    public static GimmickManager Instance {
+        get {
+            if(instance == null)
+                instance = FindObjectOfType<GimmickManager>();
+
+            return instance;
+        }
+    }
+
+    [SerializeField] List<Gimmick> gimmicks = new List<Gimmick>();
+    [SerializeField] Transform gimmickSpawnPosition = null;
+    [SerializeField] float gimmickSpawnDelayDistance = 50f;
+    public float GimmickSpawnDelayDistance => gimmmickSpawnDelayDistance;
+
+    //player에서 GimmickManager.Instance.SpawnRandomGimmick() 해서 일정 딜레이마다 호출해야됨
+    public void SpawnRandomGimmick()
+    {
+        Gimmick randomGimmick = gimmicks[Random.Range(0, gimmicks.Count)];
+        randomGimmick = PoolManager.Instance.Pop(randomGimmick.name) as Gimmick;
+
+        randomGimmick.transform.position = gimmickSpawnPosition.position;
+    }
+}
