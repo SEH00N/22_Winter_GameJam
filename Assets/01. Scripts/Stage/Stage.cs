18e561e3440a4c4272c51a426a2501bc228da544
hueam <72968569+hueam@users.noreply.github.com>
2023-01-11 17:17:32 +0900

    Stack GImmick

--- Assets/01. Scripts/Stage/Stage.cs
+++ Assets/01. Scripts/Stage/Stage.cs
@@ -6,11 +6,15 @@ public class Stage : MonoBehaviour
     private List<BallRotator> rotators = new List<BallRotator>(); 
     private BallController ballController = null;
 
+    [SerializeField] bool doInit = true;
     [SerializeField] float rotatorDetectRadius = 10f;
     public int stageIndex = 0;
 
     public void Init(int stageIndex)
     {
+        if(doInit == false)
+            return;
+
         this.stageIndex = stageIndex;
 
         ballController = transform.Find("Ball").GetComponent<BallController>();
